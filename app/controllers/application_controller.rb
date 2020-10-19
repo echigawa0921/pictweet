2938b9e968bb4db8b3861c11fca7f3d07d89a716
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-10-19 22:03:02 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,3 @@ class ApplicationController < ActionController::Base
     end
 end
 
-class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
-end
